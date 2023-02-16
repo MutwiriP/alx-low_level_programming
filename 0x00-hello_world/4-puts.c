1cc1c03d3a5ccddd1b00114fdf91f2beb4de1310
MutwiriP <mutwiriphilip@gmail.com>
2023-02-16 09:58:39 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints " Programming is like building a multilingual puzzle
+ * puzzle, followed by a new line."
+ * return 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return(0);
+}
