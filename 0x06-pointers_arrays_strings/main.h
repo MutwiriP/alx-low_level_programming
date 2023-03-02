a53104ca5edc99355dcee8952da267d532dd8ff3
MutwiriP <mutwiriphilip@gmail.com>
2023-03-02 04:43:39 -0800

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -7,9 +7,10 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
-void print_number(int n);
-#endif 
+char *string_toupper(char *n);
+char *cap_string(char *n);
+char *leet(char *n);
+char *rot13(char *n);
+
+#endif
+
