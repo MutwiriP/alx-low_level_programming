d2fd6bdfa43ff10472825b15b528816fc5c6b6dc
MutwiriP <mutwiriphilip@gmail.com>
2023-03-20 09:17:13 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include "dog.h"
-	/**
-	 * _strlen - returns the length of a string
-	 * @s: string to evaluate
-	 *
-	 * Return: the length of the string
-	 */
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
 int _strlen(char *s)
 {
 int i;
